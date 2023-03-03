cd128fe4cdae321d50b3fe61de9f04939e922655
Han Mac <phamhan@uw.edu>
2023-03-02 17:35:30 -0700

    Commit

---
+++ website/src/app/components/LandingPage.tsx
@@ -0,0 +1,16 @@
+import styles from "../styles/LandingPage.module.css";
+
+export default function LandingPage() {
+  return (
+    <div>
+      <header className={styles.header_container}>
+        <h1>
+          create<span>-web3-dapp</span>
+        </h1>
+        <p>
+          Get started by editing this page in <span>/pages/index.js</span>
+        </p>
+      </header>
+    </div>
+  );
+}
